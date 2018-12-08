dad18dadf384a2890bb063a25366dc7f41b59c29
caa52 <collins.abanda@duke.edu>
2018-12-08 15:47:22 -0500

    yuoo

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,13 +1,14 @@
+import java.util.PriorityQueue;
+
 //Kaila Lattimore kml82
 //Collins Abanda caa52
 /**
  * 
- * Although this class has a history of several years,
- * it is starting from a blank-slate, new and clean implementation
- * as of Fall 2018.
+ * Although this class has a history of several years, it is starting from a
+ * blank-slate, new and clean implementation as of Fall 2018.
  * <P>
- * Changes include relying solely on a tree for header information
- * and including debug and bits read/written information
+ * Changes include relying solely on a tree for header information and including
+ * debug and bits read/written information
  * 
  * @author Owen Astrachan
  */
@@ -16,20 +17,20 @@ public class HuffProcessor {
 
 	public static final int BITS_PER_WORD = 8;
 	public static final int BITS_PER_INT = 32;
-	public static final int ALPH_SIZE = (1 << BITS_PER_WORD); 
+	public static final int ALPH_SIZE = (1 << BITS_PER_WORD);
 	public static final int PSEUDO_EOF = ALPH_SIZE;
 	public static final int HUFF_NUMBER = 0xface8200;
-	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
+	public static final int HUFF_TREE = HUFF_NUMBER | 1;
 
 	private final int myDebugLevel;
-	
+
 	public static final int DEBUG_HIGH = 4;
 	public static final int DEBUG_LOW = 1;
-	
+
 	public HuffProcessor() {
 		this(0);
 	}
-	
+
 	public HuffProcessor(int debug) {
 		myDebugLevel = debug;
 	}
@@ -37,20 +38,18 @@ public class HuffProcessor {
 	/**
 	 * Compresses a file. Process must be reversible and loss-less.
 	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be compressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
+	 * @param in  Buffered bit stream of the file to be compressed.
+	 * @param out Buffered bit stream writing to the output file.
 	 */
-	public void compress(BitInputStream in, BitOutputStream out){  
-		
+	public void compress(BitInputStream in, BitOutputStream out) {
+
 		int[] counts = readForCounts(in);
 		HuffNode root = makeTreeFromCounts(counts);
 		String[] codings = makeCodingsFromTree(root);
-		
+
 		out.writeBits(BITS_PER_INT, HUFF_TREE);
 		writeHeader(root, out);
-		
+
 		in.reset();
 		writeCompressedBits(codings, in, out);
 		out.close();
@@ -62,11 +61,8 @@ public class HuffProcessor {
 //	}
 //	out.close();
 
+	}
 
-		}
-
-	
-	
 	private String[] makeCodingsFromTree(HuffNode root) {
 		// TODO Auto-generated method stub
 		return null;
@@ -74,12 +70,33 @@ public class HuffProcessor {
 
 	private int[] readForCounts(BitInputStream in) {
 		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	private HuffNode makeTreeFromCounts(int[] counts) {
 		// TODO Auto-generated method stub
-		return null;
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+
+
+		for(int dex = 0; dex < counts.length; dex ++) {
+		    pq.add(new HuffNode(dex, counts[dex],null,null));
+		}
+
+		while (pq.size() > 1) {
+		    HuffNode left = pq.remove();
+		    HuffNode right = pq.remove();
+		    
+		    // create new HuffNode t with weight from
+		    // left.weight+right.weight and left, right subtrees
+		    
+		    HuffNode t = new HuffNode(left.myWeight, right.myWeight, left, right);
+		    pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
+		
+		
 	}
 
 	private Object CodingsFromTree(HuffNode root) {
@@ -89,80 +106,77 @@ public class HuffProcessor {
 
 	private void writeHeader(HuffNode root, BitOutputStream out) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
 	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be decompressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
+	 * @param in  Buffered bit stream of the file to be decompressed.
+	 * @param out Buffered bit stream writing to the output file.
 	 */
-	public void decompress(BitInputStream in, BitOutputStream out){
+	public void decompress(BitInputStream in, BitOutputStream out) {
 
 		int bits = in.readBits(BITS_PER_INT);
-		if(bits != HUFF_TREE) {
+		if (bits != HUFF_TREE) {
 			throw new HuffException("illegal header starts with " + bits);
 		}
-		
+
 		HuffNode root = readTreeHeader(in);
 		readCompressedBits(root, in, out);
 		out.close();
-			
-		}
+
+	}
 
 	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
 
 		// TODO Auto-generated method stub
 		HuffNode current = root;
-		while(true) {
+		while (true) {
 			int bits = in.readBits(1);
-			if(bits == -1) {
+			if (bits == -1) {
 				throw new HuffException("bad input, no PSEUDO_EOF");
-					
-			}
-			else {
-				if(bits == 0) current = current.myLeft;
-				else current = current.myRight;
-				
-				
+
+			} else {
+				if (bits == 0)
+					current = current.myLeft;
+				else
+					current = current.myRight;
+
 			}
 			if (current.myLeft == null && current.myRight == null) {
-				if(current.myValue == PSEUDO_EOF) break;
+				if (current.myValue == PSEUDO_EOF) break;
+				
 				else {
 					out.writeBits(BITS_PER_WORD, current.myValue);
 					current = root;
 				}
 			}
 		}
-			}
-
+	}
 
 	private HuffNode readTreeHeader(BitInputStream in) {
 		// TODO Auto-generated method stub
 		int bit = in.readBits(1);
-		if(bit == -1) throw new HuffException("illegal bit, no PSEUDO_EOF");
-		if(bit == 0) {
+		if (bit == -1)
+			throw new HuffException("illegal bit, no PSEUDO_EOF");
+		if (bit == 0) {
 			HuffNode rootLeft = readTreeHeader(in);
 			HuffNode rootRight = readTreeHeader(in);
 			return new HuffNode(0, 0, rootLeft, rootRight);
-					
-		}
-		else {
-			int value = in.readBits( BITS_PER_WORD + 1);
+
+		} else {
+			int value = in.readBits(BITS_PER_WORD + 1);
 			return new HuffNode(value, 0, null, null);
-			
+
 		}
-	
-	}
 
 	}
 
+}
