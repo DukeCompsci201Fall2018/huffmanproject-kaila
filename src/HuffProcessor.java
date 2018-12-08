405261063a6800717d426f7b0dd27ee776844ef0
Owen Astrachan <kaila.lattimore@duke.edu>
2018-12-08 17:06:38 -0500

    pls

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -86,6 +86,7 @@ public class HuffProcessor {
 
 	private int[] readForCounts(BitInputStream in) {
 		int freq[] = new int[ALPH_SIZE+1];
+		freq[PSEUDO_EOF] =1;
 		while (true ) {
 		int val = in.readBits(BITS_PER_WORD);
 		if (val == -1) break;
