a1384cdf3efb5a3e6c9279e52df24d6525cdf276
Owen Astrachan <kaila.lattimore@duke.edu>
2018-12-08 20:09:05 -0500

    dfdfd

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -165,6 +165,8 @@ public class HuffProcessor {
 			out.writeBits(1, 1);
 
 			out.writeBits(BITS_PER_WORD + 1, root.myValue);
+			
+			return;
 
 		}
 	}
@@ -172,19 +174,19 @@ public class HuffProcessor {
 	
 
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
-		String code;
 		
 		while (true) {
 		int val = in.readBits(BITS_PER_WORD);
 		if (val == -1) { break;}
-		code = codings[val];
+		String code = codings[val];
+		code = codings[PSEUDO_EOF];
+	    out.writeBits(code.length(), Integer.parseInt(code,2));
 		
 		
 
 		out.writeBits(code.length(), Integer.parseInt(code,2));
 		}
-		code = codings[PSEUDO_EOF];
-	    out.writeBits(code.length(), Integer.parseInt(code,2));
+	
 
 	}
 
